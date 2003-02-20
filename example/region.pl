e7c7f0842e740242b089181fa84aeb4f6895d3eb
Thomas J Mather <geoipapi@maxmind.com>
2003-02-20 03:36:23 +0000

    added support for GeoIP Region

---
+++ example/region.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use Test;
+
+use Geo::IP;
+
+my $gi = Geo::IP->open("/usr/local/share/GeoIP/GeoIPRegion.dat", GEOIP_STANDARD);
+
+while (<DATA>) {
+  chomp;
+  my ($country, $region) = $gi->region_by_name($_);
+  print join("\t",$_,$country,$region) . "\n";
+}
+
+__DATA__
+12.10.1.4
+0.0.0.0
+66.108.94.158
+yahoo.com
+amazon.com
+4.2.144.64
