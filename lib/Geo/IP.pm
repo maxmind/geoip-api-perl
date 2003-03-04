f35db1243f1813dc01f4fdfda660a31cfea9945a
Thomas J Mather <geoipapi@maxmind.com>
2003-03-04 23:24:30 +0000

    bumped version number

--- lib/Geo/IP.pm
+++ lib/Geo/IP.pm
@@ -8,7 +8,7 @@ require DynaLoader;
 require Exporter;
 @ISA = qw(DynaLoader Exporter);
 
-$VERSION = '1.13';
+$VERSION = '1.15';
 
 bootstrap Geo::IP $VERSION;
 
@@ -124,7 +124,7 @@ http://sourceforge.net/projects/geoip/
 
 =head1 VERSION
 
-1.13
+1.15
 
 =head1 SEE ALSO
 
